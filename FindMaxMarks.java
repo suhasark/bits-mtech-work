4c9e387b05aac3881f5a1a0e932f07dceb18fd18
Suhas Krishnayya <2612srk@gmail.com>
2023-09-09 10:21:39 +0530

    DSA First Assigment Submitted

--- FindMaxMarks.java
+++ FindMaxMarks.java
@@ -1,24 +1,63 @@
 import java.util.*;
 
-public class FindMaxMarks{
+public class FindMaxMarks {
 
-public static void main(String[] args){
-    Stack<Integer> marksStack = new Stack<>();
-    marksStack.push(11);
-    marksStack.push(14);
-    marksStack.push(13);
-    marksStack.push(12);
+    public static void main(String[] args) {
+        Stack<Integer> marksStack = new Stack<>();
+        marksStack.push(11);
+        marksStack.push(14);
+        marksStack.push(13);
+        marksStack.push(12);
 
-    Queue<String> namesQueue = new ArrayDeque<String>();
-    namesQueue.add("Ricky");
-    namesQueue.add("Surya");
-    namesQueue.add("Steve");
-    namesQueue.add("Akram");
+        Queue<String> namesQueue = new ArrayDeque<String>();
+        namesQueue.add("Ricky");
+        namesQueue.add("Surya");
+        namesQueue.add("Steve");
+        namesQueue.add("Akram");
 
+        getMaxScorer(marksStack, namesQueue);
+
+        marksStack.clear();
+        namesQueue.clear();
+
+        marksStack.push(1);
+        namesQueue.add("Saurav");
+        getMaxScorer(marksStack, namesQueue);
+
+        marksStack.clear();
+        namesQueue.clear();
+
+        namesQueue.add("Rahul");
+        namesQueue.add("Sehwag");
+        namesQueue.add("Sachin");
+
+        marksStack.push(50);
+        marksStack.push(40);
+        marksStack.push(80);
+
+        getMaxScorer(marksStack, namesQueue);
+
+        marksStack.clear();
+        namesQueue.clear();
+        getMaxScorer(marksStack, namesQueue);
+
+    }
+
+private static void getMaxScorer(Stack<Integer> marksStack, Queue<String> namesQueue) {
+    try{
+    System.out.println("=========Begin Calculation======");
+    if(namesQueue.size() == 0){
+        throw new IllegalArgumentException("Empty Input, cannot calculate");
+    }
+    if(namesQueue.size() == 1){
+        System.out.println("The highest scorer is " + namesQueue.poll() + " with a score of " + marksStack.pop());
+        return;
+    }
     Queue<Integer> reversalQueue =  new ArrayDeque<>();
 
     System.out.println("Marks -- "+ marksStack);
     System.out.println("Names -- "+ namesQueue);
+
 
     while(!marksStack.isEmpty()){
         reversalQueue.add(marksStack.pop());
@@ -45,6 +84,9 @@ public static void main(String[] args){
     System.out.println("Names -- "+ namesQueue);
     System.out.println("The highest scorer is " + maxScorer + " with a score of " + maxScore);
 }
-
+finally{
+            System.out.println("=========End Calculation======");
+}
+}
 
 }
